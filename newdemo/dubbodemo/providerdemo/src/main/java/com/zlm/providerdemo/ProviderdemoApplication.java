06d3b16fcace8dd370de89aa02f9cfec28f2e5b5
zhangliming <liming.zhang@56pingtai.net>
2020-08-21 17:38:00 +0800

    20200824首次提交

---
+++ newdemo/dubbodemo/providerdemo/src/main/java/com/zlm/providerdemo/ProviderdemoApplication.java
@@ -0,0 +1,13 @@
+package com.zlm.providerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProviderdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderdemoApplication.class, args);
+    }
+
+}
